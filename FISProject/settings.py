99a398e8935befaeddfaca6c5dcf3b39223957f3
mamanyusufkhan <59729379+mamanyusufkhan@users.noreply.github.com>
2022-02-28 15:19:03 +0600

    updated

--- FISProject/settings.py
+++ FISProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,7 +57,10 @@ ROOT_URLCONF = 'FISProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'Template'],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'reacta/build'),
+            os.path.join(BASE_DIR, 'Template'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reacta/build/static'),
+]
